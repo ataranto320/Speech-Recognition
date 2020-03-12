d0ed2cf58a763dd586ab0a7cd417c3eb82bcb51d
Austin T <austin.taranto@gmail.com>
2020-03-12 16:07:14 -0400

    code up to this point plus notes on npm needed and localhost needed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("results", e => {
+    console.log(e);
+});
+
+//need npm install
+// need it to run on localhost
